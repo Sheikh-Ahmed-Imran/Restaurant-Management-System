46a8a68084eec98b525a609dc4630cd9c270369e
Mrsheikhhh <sheikhahmed45670@gmail.com>
2025-04-16 19:16:43 +0500

    'second_commit'

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
-    await mongoose.connect('mongodb+srv://az123789opp:az123789opp@ahmedkacluster.7khp1.mongodb.net/?retryWrites=true&w=majority&appName=AhmedKaCluster').then(()=>console.log("DataBase Connected"));
+    await mongoose.connect(process.env.MONGODB_URI).then(()=>console.log("DataBase Connected"));
 }
